a7643e808328b76436bd4c69adca9f66c24c6888
Rob Lovelett <rlovelett@gmail.com>
2016-12-05 20:49:39 -0600

    changed queryurl to https

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ $(document).ready(function() {
         $("#gifs").empty(); //empty the gifs div
 
         var animal = $(this).data("animal");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
         $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
